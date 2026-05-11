d7a8348f861d337e7fd0322997cfbf9e847c865a
Rafael <rafaretti2021@gmail.com>
2026-05-11 15:52:25 +0400

    Contact info screen updated and accounts is fixed...

--- Aura/Screen/Add mobile number screen/View/AddMobileNumberScreen.swift
+++ Aura/Screen/Add mobile number screen/View/AddMobileNumberScreen.swift
@@ -19,6 +19,9 @@ struct AddMobileNumberScreen: View {
     
     @State private var selectedCountry: CountryModel = .init(name: "Azerbaijan", code: "+994")
     
+    @State private var vm: MobileNumberViewModel = .init()
+    
+    @State private var selectIndex: Set<Int> = []
     
     
     private var closeButton: some View{
@@ -70,9 +73,16 @@ struct AddMobileNumberScreen: View {
             TextField("Enter mobile number", text: $number)
                 .fontModifier(size: 14, weight: .semibold, foregroundColor: colorScheme == .dark ? .colorWhite : .colorBlack)
                 .keyboardType(.numberPad)
-                .padding(.vertical, 16)
-                .padding(.horizontal, 10)
+                .padding(.vertical, 20)
+                .padding(.horizontal, 16)
                 .roundedRectangleStyle(cornerRadius: 14, backgroundColor: colorScheme == .dark ? .colorWhite.opacity(0.1) : .colorBlack.opacity(0.1), borderColor: .colorGray, borderWidth: 2)
+            
+            Text("You may receive Whatsapp and SMS notifications from us.")
+                .fontModifier(
+                    size: 16,
+                    weight: .medium,
+                    foregroundColor: .colorGray
+                )
         }
     }
     
@@ -89,16 +99,144 @@ struct AddMobileNumberScreen: View {
         return text
     }
     
-    var body: some View {
+    private var socialAccountsHeader: some View{
+        VStack(alignment: .leading, spacing: 4){
+            Text("Choose accounts for this number")
+                .fontModifier(size: 18, weight: .semibold, foregroundColor: colorScheme == .dark ? .colorWhite : .colorBlack)
+            
+            Text("Accounts you choose to add this number to will use it to help you log in and receive notifications from us")
+                .fontModifier(size: 14, weight: .medium, foregroundColor: .colorGray)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+        }
+    }
+    
+    private var accounts: some View{
+        VStack(alignment: .leading, spacing: 10){
+            ForEach(Array(vm.account.enumerated()), id: \.element.id) { index, account in
+                HStack(alignment: .top, spacing: 8){
+                    Circle()
+                        .frame(width: 30, height: 30)
+                        .contentShape(Circle())
+                        .overlay {
+                            Image(account.profilImg)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                        }
+                    
+                    VStack(alignment: .leading, spacing: 4){
+                        Text(account.userName)
+                            .fontModifier(size: 14, weight: .semibold, foregroundColor: colorScheme == .dark ? .colorWhite : .colorBlack)
+                        
+                        Text(account.socialWebName)
+                            .fontModifier(size: 14, weight: .medium, foregroundColor: .colorGray)
+                        
+                        if index == 2 && selectIndex.contains(index){
+                            Text("Adding this number will replace +994559562020 on this Instagram account.")
+                                .multilineTextAlignment(.leading)
+                                .lineLimit(3)
+                                .fontModifier(size: 12, weight: .medium, foregroundColor: .red)
+                        }
+                    }
+                    
+                   Spacer()
+                    
+                    VStack{
+                        Spacer()
+                        RoundedRectangle(cornerRadius: 4)
+                            .fill(selectIndex.contains(index) ? .backgroundBlue : .clear)
+                            .frame(width: 16, height: 16)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 4)
+                                    .stroke(selectIndex.contains(index) ? .backgroundBlue : .colorGray, lineWidth: 2)
+                            )
+                            .overlay {
+                                if selectIndex.contains(index) {
+                                    Image(.checkmark)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 8, height: 8)
+                                }
+                            }
+                            .onTapGesture {
+                                if selectIndex.contains(index) {
+                                    selectIndex.remove(index)
+                                } else {
+                                    selectIndex.insert(index)
+                                }
+                            }
+                        Spacer()
+                    }
+                }
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    if selectIndex.contains(index) {
+                           selectIndex.remove(index)
+                       } else {
+                           selectIndex.insert(index)
+                       }
+                }
+                if index != vm.account.count - 1 {
+                    Divider()
+                        .padding(.horizontal, -8)
+                }
+                
+            }
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal, 16)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .roundedRectangleStyle(cornerRadius: 12, backgroundColor: colorScheme == .dark ? .colorBlack.opacity(0.03) : .colorWhite.opacity(0.03), borderColor: .colorGray, borderWidth: 1.3)
+    }
+    
+    private var nextButton: some View{
+        Button{
+            
+        }label: {
+            Text("Next")
+                .fontModifier(size: 16, weight: .medium, foregroundColor: .colorWhite)
+                .padding(.vertical, 12)
+            
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .roundedRectangleStyle(cornerRadius: 18, backgroundColor: .backgroundBlue, borderColor: .clear, borderWidth: 0)
+    }
+    
+    private var socialAccounts: some View{
+        VStack(alignment: .leading, spacing: 8){
+            socialAccountsHeader
+            
+            accounts
+        }
+    }
+    
+    private var scrollView: some View{
         ScrollView{
-            VStack(alignment: .leading, spacing: 12){
+            VStack(alignment: .leading, spacing: 16){
                 
                 closeButton
                 
                 headerTitle
                 
                 selectCountry
+                
+                socialAccounts
+                
+                Spacer()
+                
+               
             }
+            
+        }
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10){
+            
+            scrollView
+            
+            nextButton
         }
         .sheet(isPresented: $changeCountry) {
             SelectCountryScreen(selectCountry: $selectedCountry)
